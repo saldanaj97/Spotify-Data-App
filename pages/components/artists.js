db428716e5f5947699dda16d04641e0395834273
Juan Saldana <saldanaj97@gmail.com>
2022-12-08 14:12:13 -0600

    Added dummy data and tweaked styling for each artist card

--- pages/components/artists.js
+++ pages/components/artists.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function TopArtists() {
-  let testArtists = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
+  let testArtists = ["One", "Two", "Three", "Four", "Five"];
 
   return (
     <div className='top-artists-container mx-20 flex w-72 flex-col'>
@@ -9,8 +9,13 @@ export default function TopArtists() {
       <div className='top-artists-list flex flex-col justify-evenly'>
         {testArtists.map((artist) => {
           return (
-            <div key={artist} className='card text-green-700'>
-              {artist}
+            <div key={artist} className='card flex flex-row text-black'>
+              <div className='align-center flex flex-col justify-center'>Image</div>
+              <div className='artist-info flex flex-grow flex-col justify-around'>
+                <p className='genre text-black text-opacity-50'>Genre</p>
+                <p className='artist-name'>Artist {artist}</p>
+              </div>
+              <div className='user-num-saved-songs flex flex-col justify-end '> Songs Liked </div>
             </div>
           );
         })}
