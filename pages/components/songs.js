c57bd1fde6df59ece9eec91822d41dbc5dfe2079
Juan Saldana <saldanaj97@gmail.com>
2022-12-08 14:50:16 -0600

    Implemented card layout for the top songs component with dummy data

--- pages/components/songs.js
+++ pages/components/songs.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 export default function TopSongs() {
-  let testSongs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let testSongs = ["1", "2", "3", "4", "5"];
   return (
     <div className='top-songs-container mx-20 w-72 flex-col justify-evenly'>
       <h1 className='text-center text-3xl'>Top Songs</h1>
       {testSongs.map((song) => {
-        return <div className='card text-green-700'>Song {song}</div>;
+        return (
+          <div key={song} className='card flex flex-row text-black'>
+            <div className='album-art align-center m-3 flex flex-col justify-center'>Image</div>
+            <div className='song-info flex flex-grow flex-col pt-5'>
+              <p className='artist'>Artist</p>
+              <p className='album-name'>Album</p>
+              <p className='song-name '>Song {song}</p>
+            </div>
+            <div className='user-num-saved-songs flex flex-col justify-end '> # times played </div>
+          </div>
+        );
       })}
     </div>
   );
