9c930bbbaa81d1ffeaf91e30aff225a1fd12f8cd
Juan Saldana <saldanaj97@gmail.com>
2022-12-12 15:04:16 -0600

    User auth req working

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,58 @@
-const express = require("express");
+var express = require("express");
+var request = require("request");
+const dotenv = require("dotenv");
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+
 const app = express();
 const port = 3001;
+dotenv.config();
+
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
+var redirect_uri = process.env.REDIRECT_URI;
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function (length) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+app
+  .use(express.static(__dirname + "/public"))
+  .use(cors())
+  .use(cookieParser());
+
+/**
+ * Login Route
+ * @param N/A
+ * @return N/A
+ */
+app.get("/login", function (req, res) {
+  var state = generateRandomString(16);
+  var scope = "user-read-private user-read-email";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
