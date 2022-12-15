44bdd1a7d0af75bac5fb76880811d416b3cc0f77
Juan Saldana <saldanaj97@gmail.com>
2022-12-15 15:06:23 -0600

    Top artists now displayed

--- Client/pages/components/artists.js
+++ Client/pages/components/artists.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchTopArtists } from "../api/spotify_calls";
 
-export default function TopArtists() {
-  let testArtists = ["One", "Two", "Three", "Four", "Five"];
+export default function UsersTopArtists() {
+  let [topArtists, setTopArtists] = useState([]);
+
+  useEffect(() => {
+    getTopArtists();
+  }, setTopArtists);
+
+  const getTopArtists = async () => {
+    let { items } = await fetchTopArtists();
+    setTopArtists(items);
+  };
 
   return (
-    <div className='top-artists-container mx-20 flex w-72 flex-col'>
+    <div className='top-artists-container mx-20 w-72 flex-col'>
       <h1 className='text-center text-3xl'>Top Artists</h1>
       <div className='top-artists-list flex flex-col justify-evenly'>
-        {testArtists.map((artist) => {
+        {topArtists.map((artist) => {
           return (
             <div key={artist} className='card flex flex-row text-black'>
-              <div className='align-center m-3 flex flex-col justify-center'>Image</div>
+              <img className='align-center m-3 flex h-16 w-16 flex-col justify-center' src={artist.images[2].url} />
               <div className='artist-info flex flex-grow flex-col pt-5'>
-                <p className='genre text-black text-opacity-50'>Genre</p>
-                <p className='artist-name'>Artist {artist}</p>
+                <p className='genre text-black text-opacity-50'>{artist.genres[0]}</p>
+                <p className='artist-name'>{artist.name}</p>
               </div>
-              <div className='user-num-saved-songs flex flex-col justify-end '> Songs Liked </div>
             </div>
           );
         })}
