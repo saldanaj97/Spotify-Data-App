7c0bdcee19f5943eb0eb17589c230c594e30791b
Juan Saldana <saldanaj97@gmail.com>
2023-01-10 15:11:25 -0600

    Buttons now update when a user mouses away from the album cover, as well as presses play/pause.

--- pages/components/Artists/artistTopSongs.js
+++ pages/components/Artists/artistTopSongs.js
@@ -2,13 +2,58 @@ import React, { useState, useEffect } from "react";
 import { fetchArtistDiscography } from "../../api/spotify_calls";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 
+export function PlayOrPauseButton({ track }) {
+  const [currentlyPlayingPreview, setCurrentlyPlayingPreview] = useState(false);
+  const [currentPreview, setCurrentPreview] = useState("");
+  const [oldPreview, setOldPreview] = useState("");
+
+  const previewButtonPressed = (newPreview) => {
+    /* ------- No preview currently playing -------- */
+    // Update the currentPreview and currentlyPlaying states to the newPreview and true, respectively
+    if (!currentlyPlayingPreview) {
+      setCurrentPreview(newPreview);
+      setCurrentlyPlayingPreview(true);
+      newPreview.play();
+      return;
+    }
+
+    /* ------ A preview is playing  ------- */
+    // The user has pressed the pause button on the same preview to pause
+    if (newPreview == currentPreview) {
+      newPreview.pause();
+      newPreview.currentTime = 0;
+      setCurrentlyPlayingPreview(false);
+      return;
+    }
+
+    // The user has pressed play on another preview without pressing pause on the old preview
+    setOldPreview(currentPreview);
+    setCurrentPreview(newPreview);
+    currentPreview.play();
+
+    //setTimeout(previewCompleted, 30000);
+  };
+
+  return (
+    <div
+      className='play-pause-button absolute top-0 flex w-full bg-black bg-opacity-50'
+      onClick={() => {
+        let preview = document.getElementById(track.name);
+        previewButtonPressed(preview);
+      }}
+    >
+      {currentlyPlayingPreview ? <BsPauseFill className='h-full w-full text-white' /> : <BsPlayFill className='h-full w-full text-white' />}
+    </div>
+  );
+}
+
 export default function TopArtists({ artist }) {
   const [artistDiscography, setArtistDiscography] = useState([]);
-  const [showPlayButton, setShowPlayButton] = useState(false);
+  const [previewBeingHovered, setPreviewBeingHovered] = useState(false);
 
   useEffect(() => {
     getDiscography();
-  }, [setArtistDiscography, setShowPlayButton]);
+  }, [setArtistDiscography]);
 
   // Get the artists top 5 albums and songs
   const getDiscography = async () => {
@@ -27,71 +72,57 @@ export default function TopArtists({ artist }) {
     return artists;
   };
 
-  const playPauseButton = (track) => {
-    let preview = document.getElementById(track.name);
-    if (showPlayButton) {
-      preview.play();
-    } else {
-      preview.pause();
-      preview.currentTime = 0;
-    }
-
-    //setTimeout(previewCompleted, 30000);
-  };
-
-  const AlbumTitleButton = ({ track }) => {
+  const AlbumCoverButtons = ({ track }) => {
     return (
-      <div
+      <button
         className='flex'
         onMouseEnter={() => {
-          setShowPlayButton(true);
+          setPreviewBeingHovered(true);
         }}
         onMouseLeave={() => {
-          setShowPlayButton(false);
+          setPreviewBeingHovered(false);
         }}
       >
-        <img id={track.name} src={track.album.images[0].url}></img>
+        <img id={track.name + "img"} src={track.album.images[0].url}></img>
         <video id={track.name} width='0px' height='0px' autoPlay='' name={track.name}>
           <source src={track.preview_url} type='audio/mpeg' />
         </video>
-        {showPlayButton ? (
-          <div className='play-pause-button absolute top-0 flex w-full bg-black bg-opacity-50'>
-            <BsPlayFill className='h-full w-full text-white' />
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
+        {previewBeingHovered ? <PlayOrPauseButton track={track} /> : <></>}
+      </button>
     );
   };
 
   // Create the list of the artists top 5 tracks
-  const ArtistsTopTracks = () => {
-    return artistDiscography.map((track) => {
-      return (
-        <div key={track.name} className='track m-1 flex flex-col justify-start'>
-          <div className='relative flex flex-col'>
-            <div className='track-album-buttons flex'>
-              <AlbumTitleButton track={track} />
+  const ArtistsTopTracks = ({ track }) => {
+    return (
+      <div key={track.name} className='track m-1 flex flex-col justify-start'>
+        <div className='relative flex flex-col'>
+          <div className='track-album-buttons flex'>
+            <AlbumCoverButtons track={track} />
+          </div>
+          <div className='track-info leading-tight'>
+            <p className='track-name text-left text-sm font-semibold'>{track.name}</p>
+            <div className='track-artists flex flex-row text-left text-sm'>
+              <TrackArtists track={track} />
             </div>
-            <div className='track-info leading-tight'>
-              <p className='track-name text-left text-sm font-semibold'>{track.name}</p>
-              <div className='track-artists flex flex-row text-left text-sm'>
-                <TrackArtists track={track} />
-              </div>
-              <p className='album-name text-left text-sm'>{track.album.name}</p>
-            </div>
+            <p className='album-name text-left text-sm'>{track.album.name}</p>
           </div>
         </div>
-      );
-    });
+      </div>
+    );
   };
 
   return (
     <div className='discography-container m-2 flex flex-col justify-evenly p-5'>
       <p className='top-songs-title text-left text-xl font-semibold'>Top Songs</p>
       <div className='songs-container flex flex-row'>
-        <ArtistsTopTracks />
+        {artistDiscography.map((track) => {
+          return (
+            <div>
+              <ArtistsTopTracks track={track} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
